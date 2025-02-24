1cb15df5d7773f9470040213065de8c10bda1501
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-02-24 20:28:33 +0530

    changing backend url :): fileService.ts

--- frontend/src/services/fileService.ts
+++ frontend/src/services/fileService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://backend-99l1.onrender.com';
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = process.env.API_BASE_URL;
 
 // Create an axios instance with default config
 const api = axios.create({
