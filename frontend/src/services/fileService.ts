2116b8c8b851d7dfa4305717114b3bbbd1262bd1
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-02-24 20:58:09 +0530

    changing backend url :): fileService.ts

--- frontend/src/services/fileService.ts
+++ frontend/src/services/fileService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8080";
-// const API_BASE_URL = "https://notesense-backend.onrender.com";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://notesense-backend.onrender.com";
 
 // Create an axios instance with default config
 const api = axios.create({
